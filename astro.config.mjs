9234da533be9c3ffbae0d71a2721e32193645586
Victor Chiapa <vicchiapa@gmail.com>
2024-07-08 16:37:11 -0700

    chore: Update Astro configuration and dependencies, change Node.js runtime to version 18.x

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
