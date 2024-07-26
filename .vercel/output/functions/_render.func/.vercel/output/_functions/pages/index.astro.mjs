c3fa50d602537c27da8b3f541dd8d8d87b74271f
Victor Chiapa <vicchiapa@gmail.com>
2024-07-26 11:43:18 -0700

    quitar carpeta vercel

--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/index.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('../chunks/index_BM3lhOUF.mjs');
-
-export { page };
--- .vercel/output/static/_astro/client.B0g_JweJ.js
+++
@@ -1,1 +0,0 @@
-import{c as f}from"./client.CR1qB1jm.js";export{f as default};
