f4b8d0755ddc881aeba9f8e6b5a352d34cec16b6
Victor Chiapa <vicchiapa@gmail.com>
2024-08-02 10:26:29 -0700

    scripts e info

--- src/scripts/pageLoad.ts
+++ src/scripts/pageLoad.ts
@@ -2,17 +2,17 @@
 window.addEventListener("load", () => {
    console.log("load event");
     let value = localStorage.theme as string;
-    //const div = document.querySelector("div.typing") as HTMLElement;
+    const div = document.querySelector("div.typing") as HTMLElement;
     const checkBox = document.querySelector(
        'input[type="checkbox"]'
     ) as HTMLInputElement;
     if (value == "dark") {
         //document.documentElement.classList.add("dark");
-        //div.classList.add("dark"); // para cambiar el titulo con efecto de typing
+        div.classList.add("dark"); // para cambiar el titulo con efecto de typing
         checkBox.checked = true;
      } else {
         //document.documentElement.classList.remove("dark");
-        //div.classList.remove("dark");
+        div.classList.remove("dark");
         checkBox.checked = false;
      }
 });
