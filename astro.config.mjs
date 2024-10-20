a93fde092bde7023455ebc2a67144af1cadd4cab
vik-012 <vickchiapa96@gmail.com>
2024-10-19 17:37:28 -0700

    nuevo cambio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
+// probando
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
